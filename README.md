4e7c7f8913ac1491fd6048ac68801fbb14a89267
Haste <34923485+novei@users.noreply.github.com>
2021-01-21 14:26:51 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Discohook
+
+An easy-to-use tool for building and sending Discord messages and embeds using webhooks.
+
+Live instance is available at <https://discohook.org/>.
+
+## Setup
+
+Requires Node.js 12 and the Yarn package manager installed
+
+```sh
+# Install dependencies
+yarn install
+
+# Run a development server
+yarn run dev
+
+# Run a production server
+yarn run build
+yarn run start
+```
---
+++ next.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const config = {
+  target: "serverless",
+  poweredByHeader: false,
+  distDir: "build",
+  reactStrictMode: true,
+  redirects: async () =>
+    Promise.resolve([
+      {
+        source: "/discord",
+        destination: "https://discord.gg/dtPGCsm",
+        permanent: false,
+      },
+      {
+        source: "/bot",
+        destination:
+          "https://discord.com/oauth2/authorize?client_id=633565743103082527&permissions=805694528&scope=applications.commands+bot",
+        permanent: false,
+      },
+    ]),
+}
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+const withSourceMaps = require("@zeit/next-source-maps")
+
+module.exports = withSourceMaps(withBundleAnalyzer(config))
